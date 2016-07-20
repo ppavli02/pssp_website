9f95a3f579334b200e22e49077ea81f5b1c4bdc4
Panayiotis Pavlides <ppavli02@cs.ucy.ac.cy>
2016-07-20 11:39:05 +0300

    form backup

---
+++ build_network/handleForm.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    alert("ready");
+    function customizeCarousel(){
+        var x = document.getElementById("no_layers").value;
+        alert(x);
+    }
+
+
+
+});
